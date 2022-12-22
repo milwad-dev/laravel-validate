e21df3ab5d6ee82e55aa4ba9c76b8fc8e6a66361
miladev-ent <milwad.dev@gmail.com>
2022-12-22 20:12:57 +0330

    add bitcoin_address_is_not_valid test

--- tests/ValidBitcoinAddressTest.php
+++ tests/ValidBitcoinAddressTest.php
@@ -24,15 +24,25 @@ class ValidBitcoinAddressTest extends BaseTest
      */
     public function bitcoin_address_is_valid()
     {
-        $rules = [
-            'bitcoin_address' => [new ValidBitcoinAddress()]
-        ];
-
-        $data = [
-            'bitcoin_address' => '1KFHE7w8BhaENAswwryaoccDb6qcT6DbYY'
-        ];
-
+        $rules = ['bitcoin_address' => [new ValidBitcoinAddress()]];
+        $data = ['bitcoin_address' => '1KFHE7w8BhaENAswwryaoccDb6qcT6DbYY'];
         $passes = $this->app['validator']->make($data, $rules)->passes();
+
         $this->assertTrue($passes);
+    }
+
+    /**
+     * Test bitcoin address is not valid.
+     *
+     * @test
+     * @return void
+     */
+    public function bitcoin_address_is_not_valid()
+    {
+        $rules = ['bitcoin_address' => [new ValidBitcoinAddress()]];
+        $data = ['bitcoin_address' => 'loremipsum'];
+        $passes = $this->app['validator']->make($data, $rules)->passes();
+
+        $this->assertFalse($passes);
     }
 }
