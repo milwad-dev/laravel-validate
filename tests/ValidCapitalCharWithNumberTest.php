fdde46af275d1f5d413cc8e521644076a87074fd
miladev-ent <milwad.dev@gmail.com>
2022-12-22 20:19:54 +0330

    add ValidCapitalCharWithNumberTest

---
+++ tests/ValidCapitalCharWithNumberTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Milwad\LaravelValidate\Tests;
+
+class ValidCapitalCharWithNumberTest extends BaseTest
+{
+    /**
+     * Set up.
+     *
+     * @return void
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+}
