d2185202fa7ad50d8f46484fe73f29a9e51f85d0
miladev-ent <milwad.dev@gmail.com>
2022-12-22 20:35:53 +0330

    add ValidDiscordUsernameTest & add discord_username_is_valid

---
+++ tests/ValidDiscordUsernameTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Milwad\LaravelValidate\Tests;
+
+use Milwad\LaravelValidate\Rules\ValidDiscordUsername;
+
+class ValidDiscordUsernameTest extends BaseTest
+{
+    /**
+     * Set up.
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    /**
+     * Test discord username is valid.
+     *
+     * @test
+     * @return void
+     */
+    public function discord_username_is_valid()
+    {
+        $rules = ['discord_username' => [new ValidDiscordUsername()]];
+        $data = ['discord_username' => 'Milwad#2134'];
+        $passes = $this->app['validator']->make($data, $rules)->passes();
+
+        $this->assertTrue($passes);
+    }
+}
