af2d195ff031245122c6805552c290f66e55faf8
miladev-ent <milwad.dev@gmail.com>
2022-12-22 20:27:05 +0330

    add car_number_is_valid test

--- tests/ValidCarNumberTest.php
+++ tests/ValidCarNumberTest.php
@@ -2,6 +2,8 @@
 
 namespace Milwad\LaravelValidate\Tests;
 
+use Milwad\LaravelValidate\Rules\ValidCarNumber;
+
 class ValidCarNumberTest extends BaseTest
 {
     /**
@@ -14,5 +16,18 @@ class ValidCarNumberTest extends BaseTest
         parent::setUp();
     }
 
+    /**
+     * Test car number is valid.
+     *
+     * @test
+     * @return void
+     */
+    public function car_number_is_valid()
+    {
+        $rules = ['capital_char_with_number' => [new ValidCarNumber()]];
+        $data = ['capital_char_with_number' => 'KA01AB1234'];
+        $passes = $this->app['validator']->make($data, $rules)->passes();
 
+        $this->assertTrue($passes);
+    }
 }
