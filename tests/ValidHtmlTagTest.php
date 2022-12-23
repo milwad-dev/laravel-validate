9a3959d912aecb0356b5e794286d03bcff0742e4
miladev-ent <milwad.dev@gmail.com>
2022-12-23 22:34:16 +0330

    add ValidHtmlTagTest

---
+++ tests/ValidHtmlTagTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Milwad\LaravelValidate\Tests;
+
+class ValidHtmlTagTest extends BaseTest
+{
+    /**
+     * Set up.
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+}
