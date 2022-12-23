6cc9395dd00fe6f53f1ab67a2909e17af2b37af5
miladev-ent <milwad.dev@gmail.com>
2022-12-23 22:39:05 +0330

    add ipv4_address_is_valid test

--- tests/ValidIpAddressIPV4Test.php
+++ tests/ValidIpAddressIPV4Test.php
@@ -2,6 +2,8 @@
 
 namespace Milwad\LaravelValidate\Tests;
 
+use Milwad\LaravelValidate\Rules\ValidIpAddressIPV4;
+
 class ValidIpAddressIPV4Test extends BaseTest
 {
     /**
@@ -12,5 +14,20 @@ class ValidIpAddressIPV4Test extends BaseTest
     protected function setUp(): void
     {
         parent::setUp();
+    }
+
+    /**
+     * Test ipv4 address is valid.
+     *
+     * @test
+     * @return void
+     */
+    public function ipv4_address_is_valid()
+    {
+        $rules = ['ipv4_address' => [new ValidIpAddressIPV4()]];
+        $data = ['ipv4_address' => '129.144.50.56'];
+        $passes = $this->app['validator']->make($data, $rules)->passes();
+
+        $this->assertTrue($passes);
     }
 }
