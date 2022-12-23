b769682625a33eb3527669693e3126892fcee7d5
miladev-ent <milwad.dev@gmail.com>
2022-12-23 22:36:03 +0330

    add html_tag_is_valid test

--- tests/ValidHtmlTagTest.php
+++ tests/ValidHtmlTagTest.php
@@ -2,6 +2,8 @@
 
 namespace Milwad\LaravelValidate\Tests;
 
+use Milwad\LaravelValidate\Rules\ValidHtmlTag;
+
 class ValidHtmlTagTest extends BaseTest
 {
     /**
@@ -12,5 +14,20 @@ class ValidHtmlTagTest extends BaseTest
     protected function setUp(): void
     {
         parent::setUp();
+    }
+
+    /**
+     * Test html tag is valid.
+     *
+     * @test
+     * @return void
+     */
+    public function html_tag_is_valid()
+    {
+        $rules = ['even_number' => [new ValidHtmlTag()]];
+        $data = ['even_number' => '<h1></h1>'];
+        $passes = $this->app['validator']->make($data, $rules)->passes();
+
+        $this->assertTrue($passes);
     }
 }
