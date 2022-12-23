12589b632832a0fcc24baa4b5113a7b7b636f6b0
miladev-ent <milwad.dev@gmail.com>
2022-12-23 22:30:28 +0330

    add text_is_valid_hex_code test

--- tests/ValidHexColorTest.php
+++ tests/ValidHexColorTest.php
@@ -2,6 +2,8 @@
 
 namespace Milwad\LaravelValidate\Tests;
 
+use Milwad\LaravelValidate\Rules\ValidHexColor;
+
 class ValidHexColorTest extends BaseTest
 {
     /**
@@ -12,5 +14,20 @@ class ValidHexColorTest extends BaseTest
     protected function setUp(): void
     {
         parent::setUp();
+    }
+
+    /**
+     * Test text is valid hex code.
+     *
+     * @test
+     * @return void
+     */
+    public function text_is_valid_hex_code()
+    {
+        $rules = ['even_number' => [new ValidHexColor()]];
+        $data = ['even_number' => '#fcba03'];
+        $passes = $this->app['validator']->make($data, $rules)->passes();
+
+        $this->assertTrue($passes);
     }
 }
