e464381e3a51ad8b5d148725f6ff935a28bdd979
miladev-ent <milwad.dev@gmail.com>
2022-12-23 22:50:43 +0330

    add ValidNationalCardTest

---
+++ tests/ValidNationalCardTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Milwad\LaravelValidate\Tests;
+
+class ValidNationalCardTest extends BaseTest
+{
+    /**
+     * Set up
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+
+}
