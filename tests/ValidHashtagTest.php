68c7186d004fb113aa40493d78fa61fb18a10fad
miladev-ent <milwad.dev@gmail.com>
2022-12-23 22:28:03 +0330

    add text_has_hashtag & text_has_no_hashtag tests

--- tests/ValidHashtagTest.php
+++ tests/ValidHashtagTest.php
@@ -2,6 +2,8 @@
 
 namespace Milwad\LaravelValidate\Tests;
 
+use Milwad\LaravelValidate\Rules\ValidHashtag;
+
 class ValidHashtagTest extends BaseTest
 {
     /**
@@ -12,5 +14,35 @@ class ValidHashtagTest extends BaseTest
     protected function setUp(): void
     {
         parent::setUp();
+    }
+
+    /**
+     * Test text has hashtag.
+     *
+     * @test
+     * @return void
+     */
+    public function text_has_hashtag()
+    {
+        $rules = ['even_number' => [new ValidHashtag()]];
+        $data = ['even_number' => '#laravel'];
+        $passes = $this->app['validator']->make($data, $rules)->passes();
+
+        $this->assertTrue($passes);
+    }
+
+    /**
+     * Test text has no hashtag.
+     *
+     * @test
+     * @return void
+     */
+    public function text_has_no_hashtag()
+    {
+        $rules = ['even_number' => [new ValidHashtag()]];
+        $data = ['even_number' => 'laravel'];
+        $passes = $this->app['validator']->make($data, $rules)->passes();
+
+        $this->assertFalse($passes);
     }
 }
