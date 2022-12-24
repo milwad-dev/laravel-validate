d502cad38bfc702ebd54157589cdae6ea216c66a
miladev-ent <milwad.dev@gmail.com>
2022-12-24 18:12:33 +0330

    add uuid_is_valid test

--- tests/ValidUuidTest.php
+++ tests/ValidUuidTest.php
@@ -2,6 +2,8 @@
 
 namespace Milwad\LaravelValidate\Tests;
 
+use Milwad\LaravelValidate\Rules\ValidUuid;
+
 class ValidUuidTest extends BaseTest
 {
     /**
@@ -12,5 +14,20 @@ class ValidUuidTest extends BaseTest
     protected function setUp(): void
     {
         parent::setUp();
+    }
+
+    /**
+     * Test uuid is valid.
+     *
+     * @test
+     * @return void
+     */
+    public function uuid_is_valid()
+    {
+        $rules = ['uuid' => [new ValidUuid()]];
+        $data = ['uuid' => '123e4567-e89b-12d3-a456-426655440000'];
+        $passes = $this->app['validator']->make($data, $rules)->passes();
+
+        $this->assertTrue($passes);
     }
 }
