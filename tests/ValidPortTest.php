7e70680be485746b5bd8b77abdadb74f643a824e
miladev-ent <milwad.dev@gmail.com>
2022-12-24 17:29:08 +0330

    add ValidPortTest

---
+++ tests/ValidPortTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Milwad\LaravelValidate\Tests;
+
+use Milwad\LaravelValidate\Rules\ValidPort;
+
+class ValidPortTest extends BaseTest
+{
+    /**
+     * Set up.
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+}
