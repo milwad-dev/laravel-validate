240c844e197d21d3a47ecc630b96af84e774f216
miladev-ent <milwad.dev@gmail.com>
2022-12-24 17:37:18 +0330

    add slug_is_not_valid test

--- tests/ValidSlugTest.php
+++ tests/ValidSlugTest.php
@@ -30,4 +30,19 @@ class ValidSlugTest extends BaseTest
 
         $this->assertTrue($passes);
     }
+
+    /**
+     * Test slug is not valid.
+     *
+     * @test
+     * @return void
+     */
+    public function slug_is_not_valid()
+    {
+        $rules = ['slug' => [new ValidSlug()]];
+        $data = ['slug' => 'milwad_dev'];
+        $passes = $this->app['validator']->make($data, $rules)->passes();
+
+        $this->assertFalse($passes);
+    }
 }
