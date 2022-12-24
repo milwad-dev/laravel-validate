c2740e21f06f54b9cf1dfbc31a803330ea9f4976
miladev-ent <milwad.dev@gmail.com>
2022-12-24 17:26:56 +0330

    add phone_number_is_valid test

--- tests/ValidPhoneNumberTest.php
+++ tests/ValidPhoneNumberTest.php
@@ -15,4 +15,19 @@ class ValidPhoneNumberTest extends BaseTest
     {
         parent::setUp();
     }
+
+    /**
+     * Test phone number is valid.
+     *
+     * @test
+     * @return void
+     */
+    public function phone_number_is_valid()
+    {
+        $rules = ['phone_number' => [new ValidPhoneNumber()]];
+        $data = ['phone_number' => '09366000000'];
+        $passes = $this->app['validator']->make($data, $rules)->passes();
+
+        $this->assertTrue($passes);
+    }
 }
