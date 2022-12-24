39e7730f762cb99271d87f2a8bd7155893787f75
miladev-ent <milwad.dev@gmail.com>
2022-12-24 17:40:22 +0330

    add password_is_strong test

--- tests/ValidStrongPasswordTest.php
+++ tests/ValidStrongPasswordTest.php
@@ -2,6 +2,8 @@
 
 namespace Milwad\LaravelValidate\Tests;
 
+use Milwad\LaravelValidate\Rules\ValidStrongPassword;
+
 class ValidStrongPasswordTest extends BaseTest
 {
     /**
@@ -14,5 +16,18 @@ class ValidStrongPasswordTest extends BaseTest
         parent::setUp();
     }
 
+    /**
+     * Test password is strong.
+     *
+     * @test
+     * @return void
+     */
+    public function password_is_strong()
+    {
+        $rules = ['strong_password' => [new ValidStrongPassword()]];
+        $data = ['strong_password' => 'Milwad123!'];
+        $passes = $this->app['validator']->make($data, $rules)->passes();
 
+        $this->assertTrue($passes);
+    }
 }
