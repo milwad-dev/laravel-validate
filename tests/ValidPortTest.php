ef79e80ba54d42533969a9593a6e4f6e5da4c5ab
miladev-ent <milwad.dev@gmail.com>
2022-12-24 17:29:47 +0330

    add port_is_not_valid test

--- tests/ValidPortTest.php
+++ tests/ValidPortTest.php
@@ -30,4 +30,19 @@ class ValidPortTest extends BaseTest
 
         $this->assertTrue($passes);
     }
+
+    /**
+     * Test port is not valid.
+     *
+     * @test
+     * @return void
+     */
+    public function port_is_not_valid()
+    {
+        $rules = ['port' => [new ValidPort()]];
+        $data = ['port' => '158754'];
+        $passes = $this->app['validator']->make($data, $rules)->passes();
+
+        $this->assertFalse($passes);
+    }
 }
