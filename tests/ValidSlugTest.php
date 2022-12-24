f9b60e74fba552ad647f4f5e5b8ab88c8d64f1c9
miladev-ent <milwad.dev@gmail.com>
2022-12-24 17:35:35 +0330

    add ValidSlugTest

---
+++ tests/ValidSlugTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Milwad\LaravelValidate\Tests;
+
+use Milwad\LaravelValidate\Rules\ValidSlug;
+
+class ValidSlugTest extends BaseTest
+{
+    /**
+     * Set up.
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+}
