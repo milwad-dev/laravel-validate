1ae366e62e3522849b39482a55f7939539832895
miladev-ent <milwad.dev@gmail.com>
2022-12-24 18:07:15 +0330

    add ValidUuidTest

---
+++ tests/ValidUuidTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Milwad\LaravelValidate\Tests;
+
+class ValidUuidTest extends BaseTest
+{
+    /**
+     * Set up.
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+}
