7b062a4fcbcea05371e4afc282f78adb507b137a
miladev-ent <milwad.dev@gmail.com>
2022-12-24 18:30:53 +0330

    change rule of valid base64

--- src/Rules/ValidBase64.php
+++ src/Rules/ValidBase64.php
@@ -15,7 +15,7 @@ class ValidBase64 implements Rule
      */
     public function passes($attribute, $value)
     {
-        return preg_match('/^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$/', $value);
+        return base64_encode(base64_decode($value, true)) === $value;
     }
 
     /**
