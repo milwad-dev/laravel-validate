285a322759b20443f12cd6d81904409144818906
miladev-ent <milwad.dev@gmail.com>
2022-12-24 18:05:40 +0330

    add username_is_valid test

--- tests/ValidUsernameTest.php
+++ tests/ValidUsernameTest.php
@@ -2,6 +2,8 @@
 
 namespace Milwad\LaravelValidate\Tests;
 
+use Milwad\LaravelValidate\Rules\ValidUsername;
+
 class ValidUsernameTest extends BaseTest
 {
     /**
@@ -14,5 +16,18 @@ class ValidUsernameTest extends BaseTest
         parent::setUp();
     }
 
+    /**
+     * Test username is valid.
+     *
+     * @test
+     * @return void
+     */
+    public function username_is_valid()
+    {
+        $rules = ['username' => [new ValidUsername()]];
+        $data = ['username' => 'milwad'];
+        $passes = $this->app['validator']->make($data, $rules)->passes();
 
+        $this->assertTrue($passes);
+    }
 }
