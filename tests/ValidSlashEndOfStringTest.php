584f7f896a8671c23b3eaa061653d69c2c65c787
miladev-ent <milwad.dev@gmail.com>
2022-12-24 17:31:32 +0330

    add ValidSlashEndOfStringTest

---
+++ tests/ValidSlashEndOfStringTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Milwad\LaravelValidate\Tests;
+
+use Milwad\LaravelValidate\Rules\ValidSlashEndOfString;
+
+class ValidSlashEndOfStringTest extends BaseTest
+{
+    /**
+     * Set up.
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+}
