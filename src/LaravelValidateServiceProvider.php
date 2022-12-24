8d08101ede3e90c8bd1c71fbe7424953e1b6b0d0
miladev-ent <milwad.dev@gmail.com>
2022-12-24 18:24:23 +0330

    add publish with load translations

--- src/LaravelValidateServiceProvider.php
+++ src/LaravelValidateServiceProvider.php
@@ -6,5 +6,15 @@ use Illuminate\Support\ServiceProvider;
 
 class LaravelValidateServiceProvider extends ServiceProvider
 {
+    /**
+     * Register files.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->publishes([__DIR__ . '/../validation' => lang_path('/')], 'lang');
 
+        $this->loadTranslationsFrom(__DIR__ . '/../lang', 'validation');
+    }
 }
