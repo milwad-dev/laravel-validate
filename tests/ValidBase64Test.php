9344bfb7479228fa3cd21fa877d469e1bcb96db3
miladev-ent <milwad.dev@gmail.com>
2022-12-24 18:30:36 +0330

    add base64_is_valid test

--- tests/ValidBase64Test.php
+++ tests/ValidBase64Test.php
@@ -2,6 +2,8 @@
 
 namespace Milwad\LaravelValidate\Tests;
 
+use Milwad\LaravelValidate\Rules\ValidBase64;
+
 class ValidBase64Test extends BaseTest
 {
     /**
@@ -14,5 +16,18 @@ class ValidBase64Test extends BaseTest
         parent::setUp();
     }
 
+    /**
+     * Test base64 is valid.
+     *
+     * @test
+     * @return void
+     */
+    public function base64_is_valid()
+    {
+        $rules = ['base64' => [new ValidBase64()]];
+        $data = ['base64' => 'bWlsd2Fk']; // milwad
+        $passes = $this->app['validator']->make($data, $rules)->passes();
 
+        $this->assertTrue($passes);
+    }
 }
