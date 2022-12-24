cfb8135b8d576ec9072f88cd2c6e5fc3091668c7
miladev-ent <milwad.dev@gmail.com>
2022-12-24 17:38:19 +0330

    add ValidStrongPasswordTest

---
+++ tests/ValidStrongPasswordTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Milwad\LaravelValidate\Tests;
+
+class ValidStrongPasswordTest extends BaseTest
+{
+    /**
+     * Set up.
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+
+}
