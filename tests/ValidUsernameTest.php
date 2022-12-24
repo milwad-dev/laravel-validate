603caa82f7859d8e3cf58c794dbc9d54e7ed8c77
miladev-ent <milwad.dev@gmail.com>
2022-12-24 18:06:31 +0330

    add username_is_not_valid test

--- tests/ValidUsernameTest.php
+++ tests/ValidUsernameTest.php
@@ -30,4 +30,19 @@ class ValidUsernameTest extends BaseTest
 
         $this->assertTrue($passes);
     }
+
+    /**
+     * Test username is not valid.
+     *
+     * @test
+     * @return void
+     */
+    public function username_is_not_valid()
+    {
+        $rules = ['username' => [new ValidUsername()]];
+        $data = ['username' => 'milwad!@!'];
+        $passes = $this->app['validator']->make($data, $rules)->passes();
+
+        $this->assertFalse($passes);
+    }
 }
