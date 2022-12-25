52f81da0f63c3d99c1599fb20ab299aeaa1c1a5c
miladev-ent <milwad.dev@gmail.com>
2022-12-25 17:06:01 +0330

    add validation lang (fa)

---
+++ src/lang/fa/validate.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    'base64' => 'مقدار :attribute صحیح نمی باشد.',
+    'bitcoin-address' => 'مقدار :attribute صحیح نمی باشد.',
+    'capital-char-with-number' => 'مقدار :attribute صحیح نمی باشد.',
+    'car-number' => 'مقدار :attribute صحیح نمی باشد.',
+    'cart-number-iran' => 'مقدار :attribute صحیح نمی باشد.',
+    'discord-username' => 'مقدار :attribute صحیح نمی باشد.',
+    'even-number' => 'مقدار :attribute صحیح نمی باشد.',
+    'hashtag' => 'مقدار :attribute صحیح نمی باشد.',
+    'hex-color' => 'مقدار :attribute صحیح نمی باشد.',
+    'html-tag' => 'مقدار :attribute صحیح نمی باشد.',
+    'imei' => 'مقدار :attribute صحیح نمی باشد.',
+    'ipv4' => 'مقدار :attribute صحیح نمی باشد.',
+    'ipv6' => 'مقدار :attribute صحیح نمی باشد.',
+    'jwt' => 'مقدار :attribute صحیح نمی باشد.',
+    'name-days-week' => 'مقدار :attribute صحیح نمی باشد.',
+    'national-card' => 'مقدار :attribute صحیح نمی باشد.',
+    'odd-number' => 'مقدار :attribute صحیح نمی باشد.',
+    'phone-number' => 'مقدار :attribute صحیح نمی باشد.',
+    'port' => 'مقدار :attribute صحیح نمی باشد.',
+    'slash-end-of-string' => 'مقدار :attribute صحیح نمی باشد.',
+    'slug' => 'مقدار :attribute صحیح نمی باشد.',
+    'strong-password' => 'مقدار :attribute صحیح نمی باشد.',
+    'url' => 'مقدار :attribute صحیح نمی باشد.',
+    'username' => 'مقدار :attribute صحیح نمی باشد.',
+    'uuid' => 'مقدار :attribute صحیح نمی باشد.'
+];
