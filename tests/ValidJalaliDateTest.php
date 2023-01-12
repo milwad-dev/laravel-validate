f47167e938d3dc0d741183e8485c9b7ffc10c817
miladev-ent <milwad.dev@gmail.com>
2023-01-12 16:51:57 +0330

    add ValidJalaliDateTest

---
+++ tests/ValidJalaliDateTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Milwad\LaravelValidate\Tests;
+
+class ValidJalaliDateTest extends BaseTest
+{
+    /**
+     * Set up.
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+}
