70c846f03f14d18e355e29b6584181ff85bf8c47
miladev-ent <milwad.dev@gmail.com>
2023-02-23 20:07:12 +0330

    Create ValidPattern.php

---
+++ src/Rules/ValidPattern.php
@@ -0,0 +1,31 @@
+
+<?php
+
+namespace Milwad\LaravelValidate\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class ValidPattern implements Rule
+{
+    /**
+     * Check number is odd.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        return preg_match('/^\d*[13579]$/', $value);
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return __('validate.pattern');
+    }
+}
