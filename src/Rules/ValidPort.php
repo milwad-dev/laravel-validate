780ce7b412ea0095557c72c672a1d4f9007068a6
milwad-dev <milwad-dev@users.noreply.github.com>
2023-03-03 10:48:28 +0000

    Fix styling

--- src/Rules/ValidPort.php
+++ src/Rules/ValidPort.php
@@ -9,9 +9,8 @@ class ValidPort implements Rule
     /**
      * Check port is valid.
      *
-     * @param string $attribute
-     * @param mixed  $value
-     *
+     * @param  string  $attribute
+     * @param  mixed  $value
      * @return bool
      */
     public function passes($attribute, $value)
