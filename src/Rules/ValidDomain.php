6aa2ec8ffae520ac01a315c66ff54d4d6603fdd8
Milwad <98118400+milwad-dev@users.noreply.github.com>
2023-03-11 12:14:38 +0330

    inline regex for ValidDomain

--- src/Rules/ValidDomain.php
+++ src/Rules/ValidDomain.php
@@ -15,8 +15,7 @@ class ValidDomain implements Rule
      */
     public function passes($attribute, $value)
     {
-        $pattern = '/^(?:[-A-Za-z0-9]+\.)+[A-Za-z]{2,6}$/';
-        return preg_match($pattern, $value);
+        return preg_match('/^(?:[-A-Za-z0-9]+\.)+[A-Za-z]{2,6}$/', $value);
     }
 
     /**
