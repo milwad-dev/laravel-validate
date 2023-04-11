248c4d57e69ba6736642c6f7f0b57860d9c0f924
miladev-ent <milwad.dev@gmail.com>
2023-04-11 19:27:03 +0330

    add `rules` key in config

--- config/laravel-validate.php
+++ config/laravel-validate.php
@@ -1,5 +1,12 @@
 <?php
 
-return [
+use Milwad\LaravelValidate\Rules\ValidBase64;
 
+return [
+    'rules' => [
+        'base-64' => [
+            'name' => 'base64',
+            'class' => ValidBase64::class,
+        ]
+    ]
 ];
