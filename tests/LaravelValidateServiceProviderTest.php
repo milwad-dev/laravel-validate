b4243a8a6bed3030201b2d0ab02ccf8232e61357
miladev-ent <milwad.dev@gmail.com>
2023-04-30 17:57:59 +0330

    add `all_lang_folders_publish_successfully` test

--- tests/LaravelValidateServiceProviderTest.php
+++ tests/LaravelValidateServiceProviderTest.php
@@ -6,25 +6,25 @@ use Milwad\LaravelValidate\LaravelValidateServiceProvider;
 
 class LaravelValidateServiceProviderTest extends BaseTest
 {
-    //    /**
-    //     * Test all lang folders publish successfully.
-    //     *
-    //     * @test
-    //     *
-    //     * @return void
-    //     */
-    //    public function all_lang_folders_publish_successfully()
-    //    {
-    //        $langs = (new LaravelValidateServiceProvider(app()))->langs;
-    //
-    //        foreach ($langs as $lang) {
-    //            $this->artisan('vendor:publish', [
-    //                '--tag' => "validate-lang-$lang",
-    //            ]);
-    //
-    //            $this->assertDirectoryExists(lang_path($lang));
-    //        }
-    //    }
+    /**
+     * Test all lang folders publish successfully.
+     *
+     * @test
+     *
+     * @return void
+     */
+    public function all_lang_folders_publish_successfully()
+    {
+        $langs = (new LaravelValidateServiceProvider(app()))->langs;
+
+        foreach ($langs as $lang) {
+            $this->artisan('vendor:publish', [
+                '--tag' => "validate-lang-$lang",
+            ]);
+
+            $this->assertDirectoryExists(lang_path($lang));
+        }
+    }
 
     /**
      * Test config file publish successful.
