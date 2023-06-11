82b0cd0557e4a4997581388902d9a4c50756c362
miladev-ent <milwad.dev@gmail.com>
2023-06-11 18:14:25 +0330

    Create ValidArrayKeyExistsTest.php

---
+++ tests/Rules/ValidArrayKeyExistsTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Milwad\LaravelValidate\Tests\Rules;
+
+use Milwad\LaravelValidate\Tests\BaseTest;
+
+class ValidArrayKeyExistsTest extends BaseTest
+{
+    /**
+     * Set up.
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+}
