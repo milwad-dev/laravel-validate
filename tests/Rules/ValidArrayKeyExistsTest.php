0c8f5521b4e47f9dce2ebfc18e3fcb804d2d170b
miladev-ent <milwad.dev@gmail.com>
2023-07-15 02:12:14 +0330

    Update ValidArrayKeyExistsTest.php

--- tests/Rules/ValidArrayKeyExistsTest.php
+++ tests/Rules/ValidArrayKeyExistsTest.php
@@ -2,6 +2,7 @@
 
 namespace Milwad\LaravelValidate\Tests\Rules;
 
+use Milwad\LaravelValidate\Rules\ValidArrayKeyExists;
 use Milwad\LaravelValidate\Tests\BaseTest;
 
 class ValidArrayKeyExistsTest extends BaseTest
@@ -12,5 +13,28 @@ class ValidArrayKeyExistsTest extends BaseTest
     protected function setUp(): void
     {
         parent::setUp();
+    }
+
+    /**
+     * Test array key exists is valid.
+     *
+     * @test
+     *
+     * @return void
+     */
+    public function array_key_exists_is_valid()
+    {
+        $rules = ['array' => [new ValidArrayKeyExists()]];
+        $data = ['array' => [
+            [
+                'name' => 'milwad',
+                'content' => 'laravel'
+            ]
+        ]];
+        dd((new ValidArrayKeyExists())->passes('array', $data));
+
+        $passes = $this->app['validator']->make($data, $rules)->passes();
+
+        $this->assertTrue($passes);
     }
 }
