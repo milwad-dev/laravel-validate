ffcd3974850ccffbb235b55357cf692f4f8ea512
miladev-ent <98118400+milwad-dev@users.noreply.github.com>
2023-09-04 23:25:31 +0330

    wip

--- src/LaravelValidateServiceProvider.php
+++ src/LaravelValidateServiceProvider.php
@@ -49,8 +49,6 @@ class LaravelValidateServiceProvider extends ServiceProvider
             $this->publishConfigFile();
         }
 
-        //        $this->loadValidations();
-
         $this->loadTranslationsFrom(__DIR__.'/lang', 'validation');
         $this->mergeConfigFrom(__DIR__.'/../config/laravel-validate.php', 'laravel-validate');
     }
