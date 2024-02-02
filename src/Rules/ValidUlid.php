24b75d3b5d65da6a914d3e29a82ece6b385fd83c
Kian Kamgar <kiyankamgar.k2@gmail.com>
2024-02-02 14:29:32 +0330

    Migrate ValidUlid rule to ValidationRule

--- src/Rules/ValidUlid.php
+++ src/Rules/ValidUlid.php
@@ -2,29 +2,24 @@
 
 namespace Milwad\LaravelValidate\Rules;
 
-use Illuminate\Contracts\Validation\Rule;
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
 
-class ValidUlid implements Rule
+class ValidUlid implements ValidationRule
 {
     /**
      * Check ulid.
      *
-     * @param  string  $attribute
-     * @param  mixed  $value
-     * @return bool
+     * @param string $attribute
+     * @param mixed $value
+     * @param Closure $fail
+     * @return void
      */
-    public function passes($attribute, $value)
+    public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-        return preg_match('/[0-7][0-9A-HJKMNP-TV-Z]{25}/', $value);
-    }
+        if (! preg_match('/[0-7][0-9A-HJKMNP-TV-Z]{25}/', $value)) {
 
-    /**
-     * Get the validation error message.
-     *
-     * @return string
-     */
-    public function message()
-    {
-        return __('validate.ulid');
+            $fail('validate.ulid')->translate();
+        }
     }
 }
