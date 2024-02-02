fd13d465a3b6fc3fed6da4cde37175e7a59b1800
Kian Kamgar <kiyankamgar.k2@gmail.com>
2024-02-02 21:47:33 +0330

    improve repetitive character regex in ValidNationalCard rule

--- src/Rules/ValidNationalCard.php
+++ src/Rules/ValidNationalCard.php
@@ -35,10 +35,8 @@ class ValidNationalCard implements ValidationRule
             return false;
         }
 
-        for ($i = 0; $i < 10; $i++) {
-            if (preg_match('/^'.$i.'{10}$/', $value)) {
-                return false;
-            }
+        if (preg_match('/^(.)\1*$/u', $value)) {
+            return false;
         }
 
         for ($i = 0, $sum = 0; $i < 9; $i++) {
