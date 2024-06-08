d96ca159797934a5a91a3aa9e6fe7086e9257620
Milwad Khosravi <milwad.dev@gmail.com>
2024-06-08 16:46:45 +0330

    add `DEPhoneValidator`

--- src/Utils/CountryPhoneCallback.php
+++ src/Utils/CountryPhoneCallback.php
@@ -21,14 +21,6 @@ class CountryPhoneCallback
     }
 
     /**
-     * Validate Germany phone number.
-     */
-    protected function validateDE(): false|int
-    {
-        return preg_match('/^((\+49)|(0))(1|15|16|17|19|30|31|32|33|34|40|41|42|43|44|49|151|152|153|155|156|157|159|160|162|163|180|181|182|183|184|185|186|187|188|170|171|172|173|174|175|176|177|178|179)\d{7,8}$/', $this->value);
-    }
-
-    /**
      * Validate Greece phone number.
      */
     protected function validateGR(): false|int
