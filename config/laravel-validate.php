e9b1e3cbe5bb9993948b57ca17902756e089ad3d
Milwad <98118400+milwad-dev@users.noreply.github.com>
2025-02-07 20:36:14 +0330

    Update laravel-validate.php

--- config/laravel-validate.php
+++ config/laravel-validate.php
@@ -44,4 +44,9 @@ return [
         'TR' => TRPhoneValidator::class, // Turkey
         'ZH' => ZHPhoneValidator::class, // China
     ],
+
+    /*
+     * If you want to use rules like 'required|validPhone' in your validations, you can change it to true.
+     */
+    'using_container' => false,
 ];
