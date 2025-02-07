ef8a33d8131f6861b02c170e503cff861d600478
Milwad <98118400+milwad-dev@users.noreply.github.com>
2025-02-07 19:44:52 +0330

    chrore

---
+++ tests/TestCase.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Milwad\LaravelValidate\Tests;
+
+use Milwad\LaravelValidate\LaravelValidateServiceProvider;
+
+class TestCase extends \Orchestra\Testbench\TestCase
+{
+    /**
+     * Get package providers.
+     */
+    protected function getPackageProviders($app): array
+    {
+        return [
+            LaravelValidateServiceProvider::class,
+        ];
+    }
+}
