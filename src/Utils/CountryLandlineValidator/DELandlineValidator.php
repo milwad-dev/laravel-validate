c7f4cfe4070871b0e88a6bcb97578388f5b1d773
Vahid Kaargar <vahidkaargar@gmail.com>
2025-04-17 16:53:29 +0330

    Create landline validator for germany

---
+++ src/Utils/CountryLandlineValidator/DELandlineValidator.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Milwad\LaravelValidate\Utils\CountryLandlineValidator;
+
+class DELandlineValidator implements CountryLandlineValidator
+{
+    /**
+     * Validate Germany landline numbers.
+     */
+    public function validate($value): bool
+    {
+        return preg_match('/^(?:(?:\+49|0049)[\s\-]?[2-9]\d{1,4}[\s\-]?\d{3,8}|0[2-9]\d{1,4}[\s\-]?\d{3,8})$/', $value);
+    }
+}
