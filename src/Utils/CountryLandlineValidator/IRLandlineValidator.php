5d7fd232ee25714c72640a7b249e99dd08d8d7af
Vahid Kaargar <vahidkaargar@gmail.com>
2025-04-17 16:53:51 +0330

    Create landline validator for Iran

---
+++ src/Utils/CountryLandlineValidator/IRLandlineValidator.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Milwad\LaravelValidate\Utils\CountryLandlineValidator;
+
+class IRLandlineValidator implements CountryLandlineValidator
+{
+    /**
+     * Validate Iran landline numbers.
+     */
+    public function validate($value): bool
+    {
+        return preg_match('/^(?:98|\+98|0098|0)?[1-8]{1}\d{9}$/', $value);
+    }
+}
