d7bc3de284d69b1bba95c96da6b13189aac60a90
Milwad <milwad.dev@gmail.com>
2026-05-05 16:10:41 +0330

    Create ValidLatitude.php

---
+++ src/Rules/ValidLatitude.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Milwad\LaravelValidate\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class ValidLatitude implements Rule
+{
+    /**
+     * Check base64.
+     */
+    public function passes($attribute, $value): bool
+    {
+        return base64_encode(base64_decode($value, true)) === $value;
+    }
+
+    /**
+     * Get the validation error message.
+     */
+    public function message(): string
+    {
+        return __('validate.latitude');
+    }
+}
