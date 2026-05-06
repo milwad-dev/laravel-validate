56460abc24df08e3e7d1c11036ca42134b9bf0ed
Milwad <milwad.dev@gmail.com>
2026-05-06 16:34:52 +0330

    Create ValidLongitude.php

---
+++ src/Rules/ValidLongitude.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Milwad\LaravelValidate\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class ValidLongitude implements Rule
+{
+    /**
+     * Check base64.
+     */
+    public function passes($attribute, $value): bool
+    {
+        if (! is_float($value)) {
+            return false;
+        }
+
+        return $value < -180 || $value > 180;
+    }
+
+    /**
+     * Get the validation error message.
+     */
+    public function message(): string
+    {
+        return __('validate.longitude');
+    }
+}
